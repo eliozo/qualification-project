3f9dfffb05d3988e827bf88737998ef5bf70d98e
eliozo <etebalize@gmail.com>
2022-10-24 21:18:13 +0300

    added basic flask application to start developing website

---
+++ flask-application/flaskr/__init__.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, render_template, abort, url_for, json, jsonify
+import json
+import html
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    # json 
+    @app.route("/json")
+    def getJson():
+        # read file
+        with open('C:/Users/eliz_/Documents/flask-tutorial/data/file.json', 'r') as myfile:
+            data = myfile.read()
+        return render_template('index.html', title="page", jsonfile=json.dumps(data))
+
+    # register the database commands
+
+    from flaskr import db
+    
+    db.init_app(app)
+
+    return app
