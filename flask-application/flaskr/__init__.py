247b206aab5466c6b0ec295b2d83c3ce18adee51
eliozo <etebalize@gmail.com>
2022-10-29 21:13:12 +0300

    post method added

--- flask-application/flaskr/__init__.py
+++ flask-application/flaskr/__init__.py
@@ -2,7 +2,23 @@ import os
 from flask import Flask, render_template, abort, url_for, json, jsonify
 import json
 import html
+import requests
 
+def getSPARQLtopics():
+
+    url = 'http://localhost:8080/jena-fuseki-war-4.6.1/abc/'
+    myobj = {'query': 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n'+
+    'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n'+
+    'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n'+
+    'PREFIX eozol: <http://www.dudajevagatve.lv/eozol#>\n'+
+    'SELECT * WHERE { ?sub eozol:skill ?obj . ?obj skos:prefLabel ?label . } LIMIT 10\n'
+    }
+
+    head = {'Content-Type' : 'application/x-www-form-urlencoded'}
+
+    x = requests.post(url, myobj, head)
+
+    return x.text
 
 def create_app(test_config=None):
     # create and configure the app
@@ -34,9 +50,24 @@ def create_app(test_config=None):
     @app.route("/json")
     def getJson():
         # read file
-        with open('C:/Users/eliz_/Documents/flask-tutorial/data/file.json', 'r') as myfile:
+        with open('C:/Users/eliz_/Documents/qualification-project/flask-application/data/file.json', 'r', encoding="utf-8") as myfile:
             data = myfile.read()
         return render_template('index.html', title="page", jsonfile=json.dumps(data))
+
+    @app.route('/index', methods=['GET','POST'])
+    def getIndex():
+        #with open('C:/Users/eliz_/Documents/qualification-project/flask-application/data/file.json', 'r', encoding="utf-8") as myfile:
+            #data = myfile.read()
+        #data = json.load(open('C:/Users/eliz_/Documents/qualification-project/flask-application/data/file.json', 'r', encoding='utf-8'))
+        data = json.loads(getSPARQLtopics()) 
+
+        template_context = {
+            'my_id': 'index',
+            'links': data['results']['bindings']
+        }
+    
+        return render_template('index.html', **template_context)
+
 
     # register the database commands
 
---
+++ flask-application/flaskr/cmd_post.py
@@ -0,0 +1,15 @@
+import requests
+
+url = 'http://localhost:8080/jena-fuseki-war-4.6.1/abc/'
+myobj = {'query': 'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n'+
+'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n'+
+'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>\n'+
+'PREFIX eozol: <http://www.dudajevagatve.lv/eozol#>\n'+
+'SELECT * WHERE { ?sub eozol:skill ?obj . ?obj skos:prefLabel ?label . } LIMIT 10\n'
+}
+
+head = {'Content-Type' : 'application/x-www-form-urlencoded'}
+
+x = requests.post(url, myobj, head)
+
+print(x.text)
