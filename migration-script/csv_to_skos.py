17f19cf045a67685ac38bb5bbf22d340103f93da
eliozo <etebalize@gmail.com>
2022-12-30 00:53:32 +0200

    added csv_to_skos.py file

--- migration-script/csv_to_skos.py
+++ migration-script/csv_to_skos.py
@@ -1,65 +1,76 @@
 from rdflib import Graph, Namespace, URIRef, Literal, RDF
 import csv
-import json
+import rdflib
 import requests
+
+eozol_ns = "http://www.dudajevagatve.lv/eozol#"
+
+SKOS = "http://www.w3.org/2004/02/skos/core#"
 
 def getGoogleSpreadsheet():
     URL_GOOGLE_SPREADSHEET = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQvAsYeFYhuFLmLgtMiYFeQFeeO4e0DgteRXRg1zpQ2iMcWZr-mIgdyDYnh1IoKq4l5v9C-JAE1-Qcy/pub?output=csv'
     response = requests.get(URL_GOOGLE_SPREADSHEET)
     open("spreadsheet_skos.csv", "wb").write(response.content)
 
-# def readCSVfile():
-#     result = []
-#     with open('spreadsheet_skos.csv', 'r',  encoding='utf-8') as csv_file:
-#         csv_reader = csv.reader(csv_file, delimiter=',')
-#         line_count = 0
-#         for row in csv_reader:
-#             if line_count == 0:
-#                 print(f'Column names are {", ".join(row)}')
-#                 line_count += 1
-#             else:
-#                 row_1 = row[1]
-#                 # result.append((row_1, row[3]))
-#                 print(f'\t {row_1}')
-#                 line_count += 1
-#         print(f'Processed {line_count} lines.')
-#     return result
-
-# Varbūt var arī iztikt bez konvertēšanas uz JSON un izmantot tikai CSV failu, lai pievienotu SKOS grafam utt?
-def csvToJSON(csvFilePath, jsonFilePath):
-    jsonArray = []
-
-    # Read csv file
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        # Load csv file data using csv library's dictionary reader
-        csvReader = csv.DictReader(csvf)
-
-        # Convert each csv row into python dict
-        for row in csvReader:
-            # Add this python dict to json array
-            jsonArray.append(row)
-
-    # Convert python jsonArray to JSON String and write to file
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
-        jsonString = json.dumps(jsonArray, indent=4)
-        jsonf.write(jsonString)
-
+def readCSVfile(g):
+    result = []
+    with open('spreadsheet_skos.csv', 'r',  encoding='utf-8') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            line_count += 1
+            if line_count == 1:
+                continue
+            skill_numeric_id = row[0]+'.'+row[1]+'.'+row[2]+'.'+row[3]+'.'+row[4]
+            skill_id = row[5]
+            skill_prefLabel = row[5]
+            skill_description = row[6]
+            x = skill_id.rfind(".")
+            if x == -1:
+                print("Koka sakne")
+                parent_skill_id = '' # Nav vecāka
+            else:
+                parent_skill_id = skill_id[:x]
+            addToRdfGraph(g, skill_numeric_id, skill_id, skill_description, skill_prefLabel, parent_skill_id)
+	
+# skillDescription ir string mainīgais, kurā glabājas RDF objekta vērtība
+def addToRdfGraph(g, numeric_id, skillID, skillDescription, prefLabel, parentSkill_id):
+    global eozol_ns
+    skill_node = rdflib.URIRef(eozol_ns+skillID) # RDF subjekts
+    skill_numeric_id_property = rdflib.URIRef(eozol_ns+'skillNumber') # 1 0 0 0
+    skill_id_property = rdflib.URIRef(eozol_ns+'skillIdentifier') # alg.expr
+    skill_description_property = rdflib.URIRef(SKOS+'skillDescription') # Fiksēts URL, kas apraksta RDF predikātu
+    skill_prefLabel_property = rdflib.URIRef(SKOS+'prefLabel')
+    skill_broader_property = rdflib.URIRef(SKOS+'broader')
+    skill_narrower_property = rdflib.URIRef(SKOS+'narrower')
+    skill_description_object = rdflib.term.Literal(skillDescription)
+    g.add((skill_node, skill_id_property, rdflib.term.Literal(skillID)))
+    g.add((skill_node, skill_description_property, skill_description_object))
+    g.add((skill_node, skill_numeric_id_property, rdflib.term.Literal(numeric_id)))
+    g.add((skill_node, skill_prefLabel_property, rdflib.term.Literal(prefLabel)))
+    if parentSkill_id != '':
+        parent_skill_node = rdflib.URIRef(eozol_ns+parentSkill_id)
+        g.add((skill_node, skill_broader_property, parent_skill_node)) # bērns iedur vecākam
+        g.add((parent_skill_node, skill_narrower_property, skill_node)) # vecāks iedur bērnam
+    
 
 def produceRDF(in_file, out_file):
-    SKOS = Namespace("http://www.w3.org/2004/02/skos/core#")
 
-    g = Namespace.Graph()
+    global SKOS
+
+    g = rdflib.Graph()
 
     g.bind("skos", SKOS)
+    g.bind("eozol", eozol_ns)
 
     # Opening JSON file
     f = open(in_file)
 
+    readCSVfile(g)
+
+    g.serialize(destination=out_file)
+
 if __name__ == '__main__':
     # Izsauc funkciju, kas iegūst skos dokumentu CSV faila formātā
     getGoogleSpreadsheet()
-    # Izsauc funkciju, kas lasa CSV failu un drukā noteiktu saturu no tā
-    # readCSVfile()
-    # Izsauc funkciju, kas pārveido csv failu uz json formātu
-    csvToJSON(csvFilePath="spreadsheet_skos.csv", jsonFilePath="skos.json")
-
+    produceRDF(in_file="spreadsheet_skos.csv", out_file= "skos.ttl")
