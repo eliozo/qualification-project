7d817f7339a0a4f33de9c3a69c7fff12efe993a9
Elizabete Ozolina <etebalize@gmail.com>
2024-04-15 18:25:28 +0300

    fix

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -6,7 +6,7 @@ cd /etc/our-flasks/eliozo
 pwd
 rm -fr *
 pwd
-cp -r /var/lib/jenkins/workspace/deploy-eliozo-math/qualification-project/eliozoapp/* .
+cp -r /var/lib/jenkins/workspace/deploy-eliozoapp/qualification-project/eliozoapp/* .
 sudo systemctl stop eliozo.service 
 sudo systemctl restart nginx
 sudo systemctl start eliozo.service
